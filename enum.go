e33151b2e016791536c2baea3a1c1ef8cdfbead1
FoxFurry <isacartur@gmail.com>
2022-08-24 01:11:59 +0300

    Added enum generator

--- enum.go
+++ enum.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+type EnumGenerator struct {
+	enumTag string
+}
+
+func (gen *EnumGenerator) Generate() string {
+	enumElements := strings.Split(gen.enumTag, modifierSplitter)
+
+	if len(enumElements) != 2 { // Valid enum should contain 2 elements: enum keyword and enum values
+		fmt.Printf("ignoring invalid enum tag: %s\n", gen.enumTag)
+		return ""
+	}
+
+	enumValues := strings.Split(enumElements[1], enumValueSplitter)
+
+	return enumValues[rand.Intn(len(enumValues))]
+}
