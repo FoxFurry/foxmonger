217fd92ec474cff6141e96659a9a5fbcbb23fa20
FoxFurry <isacartur@gmail.com>
2022-08-24 01:18:59 +0300

    Added email generator

--- email.go
+++ email.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"github.com/jaswdr/faker"
+)
+
+type EmailGenerator struct{}
+
+func (gen *EmailGenerator) Generate() string {
+	fakerInstance := faker.New()
+
+	return fakerInstance.Internet().Email()
+}
