90782a052bdc75115b50bf69e9f9e4308254a2e1
FoxFurry <foxfurry@FoxFurrys-MacBook-Pro.local>
2022-08-24 01:11:01 +0300

    Added fullname generator

--- fullname.go
+++ fullname.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/jaswdr/faker"
+)
+
+type FullNameGenerator struct{}
+
+func (gen *FullNameGenerator) Generate() string {
+	fakerInstance := faker.New()
+	return fakerInstance.Person().Name()
+}
