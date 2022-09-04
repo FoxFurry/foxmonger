03baeac0f872c8b3c21767d3e1016cc76aee2486
FoxFurry <isacartur@gmail.com>
2022-09-04 23:21:42 +0300

    Totally restructured project

---
+++ config.go
@@ -0,0 +1,41 @@
+// Package config
+package config
+
+var (
+// ErrTableNotFound = fmt.Errorf("table not found")
+
+)
+
+type Table struct {
+	Name           string            `mapstructure:"name"`
+	BaseMultiplier int               `mapstructure:"base_multiplier"`
+	Data           map[string]string `mapstructure:"data"`
+}
+
+type Config struct {
+	BaseCount   int     `mapstructure:"base_count"`
+	Prevalidate bool    `mapstructure:"pre_validate"`
+	DBType      string  `mapstructure:"db_type"`
+	DBName      string  `mapstructure:"db_name"`
+	DBHost      string  `mapstructure:"db_host"`
+	DBUser      string  `mapstructure:"db_user"`
+	DBPass      string  `mapstructure:"db_pass"`
+	DBPort      string  `mapstructure:"db_port"`
+	Tables      []Table `mapstructure:"tables"`
+}
+
+func (c *Config) GetTableByName(targetName string) *Table {
+	for idx := range c.Tables {
+		if c.Tables[idx].Name == targetName {
+			return &c.Tables[idx]
+		}
+	}
+
+	return nil
+}
+
+func (t *Table) GetRowByName(targetName string) string {
+	return t.Data[targetName]
+}
+
+
