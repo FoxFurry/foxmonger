958fedcdc5f1084990256d6827752ed40480d46e
FoxFurry <isacartur@gmail.com>
2022-09-04 23:22:54 +0300

    Added memstore placeholder

---
+++ memstore.go
@@ -0,0 +1,12 @@
+package foxmonger
+
+type memstore struct {
+}
+
+func (m *memstore) AddValue(row, table, value string) {
+
+}
+
+func (m *memstore) GetValues(row, table string) []string {
+	return nil
+}
