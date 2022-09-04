3b8ab432cad12a337ba9d4f1288de355ed57a176
FoxFurry <isacartur@gmail.com>
2022-09-04 23:32:05 +0300

    Removed unnecessary SplitEnum func and a bit refactored

--- internal/util/string.go
+++ internal/util/string.go
@@ -4,14 +4,8 @@ import (
 	"strings"
 )
 
-var (
-	TagSplitter = ";"
-)
+var TagSplitter = ";"
 
 func SplitTags(tagSet string) ([]string, error) {
 	return strings.Split(tagSet, TagSplitter), nil
-}
-
-func SplitEnum(enumTag string) ([]string, error) {
-
 }
