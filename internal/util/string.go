efe69fe1271674426e46c6beec7288aa3e5bbeaf
FoxFurry <isacartur@gmail.com>
2022-09-05 12:26:46 +0300

    Added Foreign key processing to utils

--- internal/util/string.go
+++ internal/util/string.go
@@ -1,11 +1,37 @@
 package util
 
 import (
+	"fmt"
 	"strings"
 )
 
 var TagSplitter = ";"
 
+type ResolvedForeign struct {
+	TargetTable,
+	TargetRow string
+}
+
 func SplitTags(tagSet string) ([]string, error) {
 	return strings.Split(tagSet, TagSplitter), nil
+}
+
+func ResolveForeignKey(foreignTag string) (*ResolvedForeign, error) {
+	foreignElements := strings.Split(foreignTag, TagSplitter)
+
+	if len(foreignElements) != 2 {
+		return nil, fmt.Errorf("tag does not contain foreign key description: %s", foreignTag)
+	}
+
+	var tableName, rowName string
+
+	_, err := fmt.Sscanf(foreignElements[1], "%s(%s)", &tableName, &rowName)
+	if err != nil {
+		return nil, fmt.Errorf("failed parsing foreign key: %w", err)
+	}
+
+	return &ResolvedForeign{
+		TargetTable: tableName,
+		TargetRow:   rowName,
+	}, nil
 }
