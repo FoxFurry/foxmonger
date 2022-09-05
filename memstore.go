dda4e4a3ec0067aa207d74af6664e67bc9b15b12
FoxFurry <isacartur@gmail.com>
2022-09-05 12:26:17 +0300

    Implemented simple memstore

--- memstore.go
+++ memstore.go
@@ -1,12 +1,23 @@
 package foxmonger
 
+import (
+	"fmt"
+)
+
 type memstore struct {
+	storage map[string][]string
 }
 
 func (m *memstore) AddValue(row, table, value string) {
-
+	RowTableKey := MergeRowTable(row, table)
+	m.storage[RowTableKey] = append(m.storage[RowTableKey], value)
 }
 
 func (m *memstore) GetValues(row, table string) []string {
-	return nil
+	RowTableKey := MergeRowTable(row, table)
+	return m.storage[RowTableKey]
+}
+
+func MergeRowTable(row, table string) string {
+	return fmt.Sprintf("%s:%s", row, table)
 }
