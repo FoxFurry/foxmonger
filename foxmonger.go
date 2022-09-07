da54ba6b75539a9c5b38ee631609ecd220d73256
FoxFurry <isacartur@gmail.com>
2022-09-08 01:54:52 +0300

    Added super simple db connection and get method for foreign keys

--- foxmonger.go
+++ foxmonger.go
@@ -1,6 +1,7 @@
 package foxmonger
 
 import (
+	"database/sql"
 	"fmt"
 	"regexp"
 
@@ -35,7 +36,7 @@ func (m *monger) PopulateDatabase() error {
 		fmt.Printf("Working on table: %s\n", table.Name)
 
 		for row, tagString := range table.Data {
-			generator, err := m.tagsToGenerator(tagString, row, table.Name, table.IsForeign(row))
+			generator, err := m.tagsToGenerator(tagString)
 			if err != nil {
 				return fmt.Errorf("failed to create row %s generator: %w", row, err)
 			}
@@ -103,9 +104,55 @@ func (m *monger) resolveTag(tagValue string) (any, error) {
 			return nil, fmt.Errorf("could not create foreign key producer: %w", err)
 		}
 
-		return tag.NewForeignProducer(nil), nil
+		foreignRows, err := m.getRows(foreignTarget.TargetTable, foreignTarget.TargetRow)
+		if err != nil {
+			return nil, fmt.Errorf("could not get foreign target rows: %w", err)
+		}
+
+		return tag.NewForeignProducer(foreignRows), nil
 	default:
 		return nil, fmt.Errorf("unknown tag: %s", tagValue)
+	}
+}
+
+func (m *monger) getRows(tableName, rowName string) ([]string, error) {
+	query := fmt.Sprintf("SELECT %s FROM %s", rowName, tableName)
+
+	db, err := m.openConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var (
+		rowsResult []string
+		rowBuffer  any
+	)
+
+	for rows.Next() {
+		if err := rows.Scan(&rowBuffer); err != nil {
+			panic("this should not have happened: " + err.Error())
+		}
+
+		rowsResult = append(rowsResult, fmt.Sprintf("%v", rowBuffer))
+	}
+
+	return rowsResult, nil
+}
+
+func (m *monger) openConnection() (*sql.DB, error) {
+	switch m.conf.DBType {
+	case MySQLType:
+		return sql.Open(MySQLType, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", m.conf.DBUser, m.conf.DBPass, m.conf.DBHost, m.conf.DBPort, m.conf.DBName))
+	case PostgreSQL:
+		return nil, fmt.Errorf("%s is not supported yet", m.conf.DBType)
+	default:
+		return nil, fmt.Errorf("unknown db type: %s", m.conf.DBType)
 	}
 }
 
