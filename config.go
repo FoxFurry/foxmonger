38ec62d4f6125c1341bd8d65249ecf628cdbe60d
FoxFurry <isacartur@gmail.com>
2023-06-17 08:27:42 +0300

    add batch size to config.go

--- config.go
+++ config.go
@@ -3,6 +3,7 @@ package foxmonger
 type Table struct {
 	Name           string            `mapstructure:"name"`
 	BaseMultiplier int               `mapstructure:"base_multiplier"`
+	BatchSize      int               `mapstructure:"batch_size"`
 	Data           map[string]string `mapstructure:"data"`
 	ExportQueries  bool              `mapstructure:"export_queries"`
 	ExportPath     string            `mapstructure:"export_path"`
@@ -10,13 +11,14 @@ type Table struct {
 }
 
 type Config struct {
-	BaseCount   int     `mapstructure:"base_count"`
-	Prevalidate bool    `mapstructure:"pre_validate"`
-	DBType      string  `mapstructure:"db_type"`
-	DBName      string  `mapstructure:"db_name"`
-	DBHost      string  `mapstructure:"db_host"`
-	DBUser      string  `mapstructure:"db_user"`
-	DBPass      string  `mapstructure:"db_pass"`
-	DBPort      string  `mapstructure:"db_port"`
-	Tables      []Table `mapstructure:"tables"`
+	BaseCount        int     `mapstructure:"base_count"`
+	Prevalidate      bool    `mapstructure:"pre_validate"`
+	DefaultBatchSize int     `mapstructure:"default_batch_size"`
+	DBType           string  `mapstructure:"db_type"`
+	DBName           string  `mapstructure:"db_name"`
+	DBHost           string  `mapstructure:"db_host"`
+	DBUser           string  `mapstructure:"db_user"`
+	DBPass           string  `mapstructure:"db_pass"`
+	DBPort           string  `mapstructure:"db_port"`
+	Tables           []Table `mapstructure:"tables"`
 }
