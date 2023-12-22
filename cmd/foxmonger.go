acfad8db9bb48b3a39ea0c7bf85d451eb22e7ca0
FoxFurry <isacartur@gmail.com>
2023-12-22 13:05:19 +0200

    version overhaul

--- cmd/foxmonger.go
+++ cmd/foxmonger.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"flag"
 	"fmt"
 	"os"
 
 	"github.com/FoxFurry/foxmonger"
-	"github.com/spf13/viper"
+	"github.com/FoxFurry/foxmonger/internal/database"
 )
 
 const (
@@ -18,45 +19,66 @@ const (
 var (
 	flags = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 
-	config = flags.String("config", "", "File containing SQL configuration for monger. Find more about it on official github page")
-	help   = flags.Bool("help", false, "Test")
+	help = flags.Bool("help", false, "TBD")
+
+	dbType = flags.String("type", "", "TBD")
+	dbName = flags.String("db", "", "TBD")
+	dbUser = flags.String("user", "", "TBD")
+	dbPass = flags.String("pass", "", "TBD")
+	dbHost = flags.String("host", "", "TBD")
+	dbPort = flags.String("port", "", "TBD")
 )
 
 func main() {
-	flags.Parse(os.Args[1:])
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		osError("failed reading flags: %v\n", err)
+	}
 
 	if *help {
 		usage()
 		os.Exit(0)
 	}
 
-	if *config == "" {
-		osError("config is mandatory for execution\n")
+	if err := checkFlags(dbType, dbName, dbUser, dbPass, dbHost, dbPort); err != nil {
+		osError("error: %v\n", err)
 	}
 
-	viper.SetConfigFile(*config)
-	if err := viper.ReadInConfig(); err != nil {
-		osError("failed to read config: %v\n", err)
-	}
-
-	conf := new(foxmonger.Config)
-
-	if err := viper.Unmarshal(conf); err != nil {
-		osError("failed to unmarshal config: %v\n", err)
-	}
-
-	db, err := openConnection(conf)
+	targetDB, err := openConnection(*dbType, *dbUser, *dbPass, *dbHost, *dbPort, *dbName)
 	if err != nil {
 		osError("failed to open db connection: %v\n", err)
 	}
 
-	monger := foxmonger.NewMonger(conf, db)
+	db := database.NewDatabase(targetDB)
 
-	if err := monger.PopulateDatabase(); err != nil {
+	monger := foxmonger.NewMonger(db)
+	if err := monger.PopulateDatabase(context.Background()); err != nil {
 		osError("failed to populate db: %v\n", err)
 	}
 
 	return
+}
+
+func checkFlags(dbType, dbName, dbUser, dbPass, dbHost, dbPort *string) error {
+	switch "" {
+	case *dbType:
+		return isMissingError("type")
+	case *dbName:
+		return isMissingError("name")
+	case *dbUser:
+		return isMissingError("user")
+	case *dbPass:
+		return isMissingError("pass")
+	case *dbHost:
+		return isMissingError("host")
+	case *dbPort:
+		return isMissingError("port")
+	}
+
+	return nil
+}
+
+func isMissingError(missing string) error {
+	return fmt.Errorf("flag '%s' is not set", missing)
 }
 
 func usage() {
@@ -76,13 +98,6 @@ func osError(format string, opts ...any) {
 	os.Exit(1)
 }
 
-func openConnection(conf *foxmonger.Config) (*sql.DB, error) {
-	switch conf.DBType {
-	case mysqlType:
-		return sql.Open(mysqlType, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", conf.DBUser, conf.DBPass, conf.DBHost, conf.DBPort, conf.DBName))
-	case postgresqlType:
-		return nil, fmt.Errorf("%s is not supported yet", conf.DBType)
-	default:
-		return nil, fmt.Errorf("unknown db type: %s", conf.DBType)
-	}
+func openConnection(dbType, dbUser, dbPass, dbHost, dbPort, dbName string) (*sql.DB, error) {
+	return sql.Open(dbType, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName))
 }
